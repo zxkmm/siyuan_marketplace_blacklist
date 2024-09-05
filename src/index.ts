1d943d9af3acda9fcf38548c6ee3e95f34619c1f
zxkmm <zxkmm@hotmail.com>
2024-09-05 20:41:35 +0800

    add block btn fully works, but TODO remove css before add it again

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,16 @@ const STORAGE_NAME = "menu-config";
 
 var packageNameClass = document.getElementsByClassName("ft__on-surface");
 
+enum BlockItemType {
+  USER = "USER",
+  REPO = "REPO",
+}
+
+enum BlockItemTypeSettingKey {
+  USER = "USER",
+  REPO = "REPO",
+}
+
 export default class siyuan_rmv_btn extends Plugin {
   private settingUtils: SettingUtils;
 
@@ -35,7 +45,7 @@ export default class siyuan_rmv_btn extends Plugin {
   rmvMarketPlaceCardsByNameJs(_toRemoveListArray_) {
     var siyuanMarketPlaceObserver = new MutationObserver(function (
       mutationsList,
-      observer
+      observer,
     ) {
       for (var i = 0; i < packageNameClass.length; i++) {
         if (_toRemoveListArray_.includes(packageNameClass[i].textContent)) {
@@ -70,7 +80,7 @@ export default class siyuan_rmv_btn extends Plugin {
       _arr_with_css_.push(
         `.b3-card.b3-card--wrap[data-obj*='"name":"${_toRemovePackageNameListArray_[i]}"'] {
             display: none;
-        }`
+        }`,
       );
     }
 
@@ -90,12 +100,11 @@ export default class siyuan_rmv_btn extends Plugin {
 
     var _arr_with_css_ = [];
 
-
     for (var i = 0; i < _toRemoveGitHubUsernameListArray_.length; i++) {
       _arr_with_css_.push(
         `.b3-card.b3-card--wrap[data-obj*='"repoURL":"https://github.com/${_toRemoveGitHubUsernameListArray_[i]}/'] {
             display: none;
-        }`
+        }`,
       );
     }
 
@@ -103,6 +112,257 @@ export default class siyuan_rmv_btn extends Plugin {
 
     for (var i = 0; i < _arr_with_css_.length; i++) {
       this.applyStyles(_arr_with_css_[i]);
+    }
+  }
+
+  b3cardClickListener() {
+    console.log("called");
+    const observer = new MutationObserver((mutationsList, observer) => {
+      for (let mutation of mutationsList) {
+        if (mutation.type === "childList") {
+          mutation.addedNodes.forEach((node) => {
+            if (node.classList && node.classList.contains("b3-card")) {
+              this.addClickListener(node);
+            } else if (node.querySelectorAll) {
+              const cards = node.querySelectorAll(".b3-card");
+              cards.forEach((card) => this.addClickListener(card));
+            }
+          });
+        }
+      }
+    });
+
+    const config = { childList: true, subtree: true };
+
+    observer.observe(document.body, config);
+
+    // add for current existing b3cards once anyway
+    const initialCards = document.querySelectorAll(".b3-card");
+    initialCards.forEach((card) => this.addClickListener(card));
+  }
+
+  addClickListener(element) {
+    element.addEventListener("click", () => {
+      console.log("click card callback");
+      for (let i = 0; i < 20; i++) {
+        setTimeout(() => this.addBlockButton(), i * 50);
+      }
+    });
+  }
+
+  checkIfItsCorrectPage() {
+    const link = document.querySelector(
+      'a[target="_blank"][class="ft__on-surface ft__smaller"][title="GitHub Repo"]',
+    );
+    if (link) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  fetchGithubUserAndRepoForCurrentDisplayItem() {
+    const links = document.querySelectorAll(
+      'a[target="_blank"][title="GitHub Repo"]',
+    );
+
+    for (let i = 0; i < links.length; i++) {
+      const link = links[i];
+      const href = link.getAttribute("href");
+      if (href) {
+        const match = href.match(/https:\/\/github\.com\/([^/]+)\/([^/]+)/);
+        if (match) {
+          const user = match[1];
+          const repo = match[2];
+          console.log(`USER: ${user}, REPO: ${repo}`);
+          break;
+        }
+      }
+    }
+  }
+
+  fetchGithubUserForCurrentDisplayItem() {
+    const links = document.querySelectorAll(
+      'a[target="_blank"][title="GitHub Repo"]',
+    );
+
+    for (let i = 0; i < links.length; i++) {
+      const link = links[i];
+      const href = link.getAttribute("href");
+      if (href) {
+        const match = href.match(/https:\/\/github\.com\/([^/]+)\/([^/]+)/);
+        if (match) {
+          const user = match[1];
+          return user;
+          // console.log(`USER: ${user}`);
+          // break;
+        }
+      }
+    }
+  }
+
+  fetchGithubRepoForCurrentDisplayItem() {
+    const links = document.querySelectorAll(
+      'a[target="_blank"][title="GitHub Repo"]',
+    );
+
+    for (let i = 0; i < links.length; i++) {
+      const link = links[i];
+      const href = link.getAttribute("href");
+      if (href) {
+        const match = href.match(/https:\/\/github\.com\/([^/]+)\/([^/]+)/);
+        if (match) {
+          const repo = match[2];
+          return repo;
+          // console.log(`REPO: ${repo}`);
+          // break;
+        }
+      }
+    }
+  }
+
+  // export function backToList() {
+  //   const backBtn = document.querySelector('div[data-type="goBack"]');
+  //   if (backBtn) {
+  //     backBtn.click();
+  //   }
+  // }
+
+  backToList() {
+    const backBtn = document.querySelector('div[data-type="goBack"]');
+    if (backBtn && backBtn instanceof HTMLElement) {
+      backBtn.click();
+    }
+  }
+
+  addBlockButton() {
+    const existingButtons = document.querySelectorAll(
+      '[zxkmm_global_identifier="new_added_element"]',
+    );
+
+    if (existingButtons.length > 0) {
+      console.log("Elements already exist, removing and re-adding");
+      console.log(existingButtons);
+      existingButtons.forEach((elem) => elem.remove());
+    }
+
+    console.log("add btn");
+    console.log("222");
+    const feedbackButton = document.querySelector('a[data-type="feedback"]'); // feed back icon
+    console.log("feedbackbtn", feedbackButton);
+
+    if (feedbackButton) {
+      const blockPluginBtn = document.createElement("a");
+      blockPluginBtn.title = "集市黑名单：拉黑这个插件";
+      blockPluginBtn.className = "b3-button b3-button--blacklist"; //TODO: write and use another btn css
+      blockPluginBtn.style.width = "168px";
+      blockPluginBtn.textContent = "拉黑插件";
+      blockPluginBtn.setAttribute(
+        "zxkmm_global_identifier",
+        "new_added_element",
+      );
+
+      const blockAuthorBtn = document.createElement("a");
+      blockAuthorBtn.title = "集市黑名单：拉黑这个作者";
+      blockAuthorBtn.className = "b3-button b3-button--blacklist";
+      blockAuthorBtn.style.width = "168px";
+      blockAuthorBtn.textContent = "拉黑作者";
+      blockAuthorBtn.setAttribute(
+        "zxkmm_global_identifier",
+        "new_added_element",
+      );
+
+      ///v callback listener and worker
+      blockPluginBtn.onclick = (event: Event) => {
+        event.preventDefault();
+        // idk how siyuan listen the click callback
+        // but just in case and also in case it changed in the future....
+        console.log("click callback TODO TODO TODO!!");
+        const userName = this.fetchGithubRepoForCurrentDisplayItem();
+        this.appendCurrentItemIntoList(BlockItemType.REPO, userName);
+      };
+
+      blockAuthorBtn.onclick = (event: Event) => {
+        event.preventDefault(); //
+        console.log("click callback TODO TODO TODO!!");
+        const userName = this.fetchGithubUserForCurrentDisplayItem();
+        this.appendCurrentItemIntoList(BlockItemType.USER, userName);
+      };
+      ///^
+
+      const separator = document.createElement("div");
+      separator.className = "fn__hr--b";
+      separator.setAttribute("zxkmm_global_identifier", "new_added_element");
+
+      const separator1 = document.createElement("div");
+      separator1.className = "fn__hr--b";
+      separator1.setAttribute("zxkmm_global_identifier", "new_added_element");
+
+      const parentDiv = feedbackButton.parentElement;
+      console.log("parentDiv:", parentDiv);
+
+      if (parentDiv) {
+        console.log("addded btnnn");
+        // feed back btn pos
+        parentDiv.appendChild(separator);
+        parentDiv.appendChild(blockPluginBtn);
+        parentDiv.appendChild(separator1);
+        parentDiv.appendChild(blockAuthorBtn);
+        //repo etc pos
+      }
+    }
+  }
+
+  appendCurrentItemIntoList(_block_type_: BlockItemType, _block_name_: string) {
+    try {
+      // 注意await
+      var enableDeviceList;
+      switch (_block_type_) {
+        case BlockItemType.REPO:
+          enableDeviceList = this.settingUtils.get("pluginBlacklist");
+          break;
+        case BlockItemType.USER:
+          enableDeviceList = this.settingUtils.get("authorBlacklist");
+          break;
+      }
+
+      var blockListeArray = enableDeviceList.split(",");
+      var blockListArrayLength = blockListeArray.length;
+      var blockListArrayLast = blockListeArray[blockListArrayLength - 1];
+
+      // remove empty line
+      if (blockListArrayLast === "") {
+        blockListeArray.pop();
+      }
+
+      blockListeArray.push(_block_name_);
+
+      var blockListArrayString = blockListeArray.join(",");
+
+      switch (_block_type_) {
+        case BlockItemType.USER:
+          this.settingUtils.assignValue(
+            "authorBlacklist",
+            blockListArrayString,
+          );
+          this.refreshCss();
+          showMessage(`已拉黑${_block_name_}的所有插件`);
+          this.backToList();
+          break;
+        case BlockItemType.REPO:
+          this.settingUtils.assignValue(
+            "pluginBlacklist",
+            blockListArrayString,
+          );
+          this.refreshCss();
+          showMessage(`已拉黑${_block_name_}插件`);
+          this.backToList();
+          break;
+      }
+
+      this.settingUtils.save();
+    } catch (error) {
+      console.error(`Error adding current ${_block_type_} into list:`, error);
     }
   }
 
@@ -123,15 +383,15 @@ export default class siyuan_rmv_btn extends Plugin {
     });
 
     this.settingUtils.addItem({
-        key: "hideMethod",
-        value: 1,
-        type: "select",
-        title: this.i18n.hideMethodTitle,
-        description: this.i18n.hideMethodDesc,
-        options: {
-            1: "CSS",
-            2: "JS listener",
-        },
+      key: "hideMethod",
+      value: 1,
+      type: "select",
+      title: this.i18n.hideMethodTitle,
+      description: this.i18n.hideMethodDesc,
+      options: {
+        1: "CSS",
+        2: "JS listener",
+      },
     });
     this.settingUtils.addItem({
       key: "pluginBlacklist",
@@ -163,38 +423,73 @@ export default class siyuan_rmv_btn extends Plugin {
     });
   }
 
+  refreshCss() {
+    if (this.settingUtils.get("totalSwitch")) {
+      const _hideMethods_ = this.settingUtils.get("hideMethod");
+
+      // console.log(typeof _hideMethods_);
+      // fuck stupid type 巨坑，草
+
+      switch (_hideMethods_) {
+        case "1":
+          // console.log("hide by CSS");
+          this.rmvMarketPlaceCardsByNameCss(
+            this.convertStringToArray(this.settingUtils.get("pluginBlacklist")),
+          );
+
+          this.rmvMarketPlaceCardsByGitHubUsernameCss(
+            this.convertStringToArray(this.settingUtils.get("authorBlacklist")),
+          );
+          break;
+        case "2":
+          // console.log("hide by JS listener");
+          this.rmvMarketPlaceCardsByNameJs(
+            this.convertStringToArray(this.settingUtils.get("pluginBlacklist")),
+          );
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
   onLayoutReady() {
+    // const pollInterval = setInterval(() => {
+    //   console.log("123123123");
+    //   addBlockButton();
+    // }, 1000);
+
     this.loadData(STORAGE_NAME);
     this.settingUtils.load();
 
     if (this.settingUtils.get("totalSwitch")) {
+      const _hideMethods_ = this.settingUtils.get("hideMethod");
 
-        const _hideMethods_ = this.settingUtils.get("hideMethod");
+      // console.log(typeof _hideMethods_);
+      // fuck stupid type 巨坑，草
 
-        // console.log(typeof _hideMethods_);
-        // fuck stupid type 巨坑，草
+      switch (_hideMethods_) {
+        case "1":
+          // console.log("hide by CSS");
+          this.rmvMarketPlaceCardsByNameCss(
+            this.convertStringToArray(this.settingUtils.get("pluginBlacklist")),
+          );
 
+          this.rmvMarketPlaceCardsByGitHubUsernameCss(
+            this.convertStringToArray(this.settingUtils.get("authorBlacklist")),
+          );
+          break;
+        case "2":
+          // console.log("hide by JS listener");
+          this.rmvMarketPlaceCardsByNameJs(
+            this.convertStringToArray(this.settingUtils.get("pluginBlacklist")),
+          );
+          break;
+        default:
+          break;
+      }
 
-        switch (_hideMethods_) {
-            case "1":
-                // console.log("hide by CSS");
-                this.rmvMarketPlaceCardsByNameCss(
-                    this.convertStringToArray(this.settingUtils.get("pluginBlacklist"))
-                );
-
-                this.rmvMarketPlaceCardsByGitHubUsernameCss(
-                    this.convertStringToArray(this.settingUtils.get("authorBlacklist"))
-                );
-                break;
-            case "2":
-                // console.log("hide by JS listener");
-                this.rmvMarketPlaceCardsByNameJs(
-                    this.convertStringToArray(this.settingUtils.get("pluginBlacklist"))
-                );
-                break;
-            default:
-                break;
-        }
+      this.b3cardClickListener();
     }
   }
 
